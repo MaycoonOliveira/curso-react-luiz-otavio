cb3961e2bb78294ab5884a51ec1d9c929de27f0c
Maycon de Oliveira <maycon009silv@gmail.com>
2025-03-04 13:33:51 -0300

    Curso React

---
+++ src/components/TextInput/index.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+export const TextInput = ({ searchValue, handleChange }) => {
+  return (
+    <input
+      className="text-input"
+      onChange={handleChange}
+      type="search"
+      value={searchValue}
+      placeholder="Type your search"
+    />
+  );
+};
